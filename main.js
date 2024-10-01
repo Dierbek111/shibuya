93d4b96b3aa5cdef6adebc41ca786ac7056a93c2
Dierbek111 <dierbekrahmatullaev00@gmail.com>
2024-10-01 16:04:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const swiperAlbum1 = new Swiper('.swiper-album-1', {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+    allowTouchMove: false,
+
+    speed: 20000
+});
+
+
+const swiperAlbum2 = new Swiper('.swiper-album-2', {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+        reverseDirection: true
+    },
+    allowTouchMove: false,
+
+    speed: 20000
+});
+
+
